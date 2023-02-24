e7a06e71cf54230424c8a8a2404608823eee4a86
Nuria Castaño <codesalnuria@gmail.com>
2023-02-24 12:31:19 +0100

    add condition for indexCocktail 0 o -1

--- src/js/main.js
+++ src/js/main.js
@@ -33,15 +33,23 @@ const renderListCocktail = (listCocktailData) => {
   eventToCocktail();
 };
 
+//función pinta la lista de favoritos
+
+const renderFavListCocktail = (listFavCocktail) => {
+  listCocktailFav.innerHTML = "";
+  for (const item of listFavCocktail) {
+    listCocktailFav.innerHTML += renderCocktail(item);
+  }
+};
+
 //función para pintar un cóctel
 const renderCocktail = (cocktail) => {
   if (!cocktail.strDrinkThumb) {
     cocktail.strDrinkThumb = placeholder;
   }
   let html = "";
-  html += `<li class="js_liElement"> 
-  <article id=
-  "${cocktail.idDrink}">
+  html += `<li> 
+  <article class="js_liElement" id="${cocktail.idDrink}">
     <img class="img" src="${cocktail.strDrinkThumb}" alt="Foto Cocktail ${cocktail.strDrink}"> 
     <h3>${cocktail.strDrink}</h3>
   </article>
@@ -71,13 +79,35 @@ btnSearch.addEventListener("click", handleClickSearch);
 
 //3- Favoritos: 3.1-seleccionar todos los elementos de la lista: añadir una clase en todos los li. 3.2-
 
+
+//función manejadora de eventToCocktail
+const handleClickCocktail = (ev) => {
+    console.log("Me has seleccionado");
+    console.log(ev.currentTarget.id);
+    ev.currentTarget.classList.toggle('selected');
+    const idSelected = ev.currentTarget.id;
+    //buscar el objeto del coctel seleccionado en toda la lista de cocteles
+    const cocktailSelected = listCocktailData.find(cocktail=>cocktail.idDrink === idSelected);
+    console.log(cocktailSelected);
+    //buscar la posición del coctel en mi lista de favoritos. si es -1 no está en esa lista y hay que ponerlo
+    const indexCocktail= listFavCocktail.findIndex(cocktail=>cocktail.idDrink===idSelected);
+    console.log(indexCocktail);
+    if(indexCocktail === -1){
+      listFavCocktail.push(cocktailSelected);
+      console.log(listFavCocktail);
+    } else {
+      listFavCocktail.splice(indexCocktail,1);
+     }
+     const indexCocktailPost= listFavCocktail.findIndex(cocktail=>cocktail.idDrink===idSelected);
+     console.log(indexCocktailPost);
+    //pintar la lista de favoritos con función renderFavListCocktail
+    renderFavListCocktail(listFavCocktail);
+  };
+
+
 //función que se ejecuta una vez cogidos los datos del servidor e incluye un evento click en cada coctel 
 const eventToCocktail = () => {
-  const handleClickCocktail = (ev) => {
-    console.log("Me has seleccionad");
-  };
   const liElementCocktail = document.querySelectorAll(".js_liElement"); //elementos de li
-  console.log(liElementCocktail);
   //hacer bucle para que me seleccione todos los elementos y añadirle un evento click a cada uno de ellos
   for (const item of liElementCocktail) {
     item.addEventListener("click", handleClickCocktail);
