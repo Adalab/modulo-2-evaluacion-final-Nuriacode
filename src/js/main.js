9e3d52d21853c8fb5ecd79c8033d82d5f80ef26f
Nuria Castaño <codesalnuria@gmail.com>
2023-02-24 08:49:30 +0100

    add conditional placeholder

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,67 @@
-'use strict';
+"use strict";
 
 //Variables
 //Obtener los datos del servidor
 //Pintar los datos del cóctel (imagen y nombre)
+const descCocktail = document.querySelector(".js_descSearch"); //cóctel que busco
+const placeholder = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+const urlmargarita = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`;
+const btnSearch = document.querySelector(".js_btnSearch"); //botón de buscar
+const listCocktail = document.querySelector(".js_listCocktail"); //lista de cocteles que vienen del servidor
+let listCocktailData = []; //array de cocteles
 
-const descCocktail = document.querySelector('.js_descSearch'); //cóctel que busco
-const btnSearch = document.querySelector('.js_btnSearch'); //botón de buscar
-const listCocktail = document.querySelector('.js_listCocktail'); //lista de cocteles que vienen del servidor
-let listCocktailData=[];
+//1-Llamada al servidor para que salga de primeras la lista de margaritas
+fetch(urlmargarita)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    listCocktailData = data.drinks;
+    console.log(listCocktailData);
 
-//crear función render de los coteles que va a ir detrás dentro de handleclick
+    renderListCocktail(listCocktailData);
+  });
 
+//crear función pinta la lista de los cocteles
+const renderListCocktail = (listCocktailData) => {
+  listCocktail.innerHTML = "";
+  for (const cocktail of listCocktailData) {
+    listCocktail.innerHTML += renderCocktail(cocktail);
+  }
+};
 
-
+//función para pintar un cóctel
+const renderCocktail = (cocktail) => {
+   console.log(cocktail);
+   if(!cocktail.strDrinkThumb){
+     cocktail.strDrinkThumb=placeholder;
+   }
+  let html = "";
+  html += `<li> 
+  <article id=
+  "${cocktail.idDrink}">
+    <img src="${cocktail.strDrinkThumb}" alt="Foto Cocktail ${cocktail.strDrink}"> 
+    <h3>${cocktail.strDrink}</h3>
+  </article>
+  </li>`;
+  return html;
+};
 
 //función manejadora de click buscar
 const handleClickSearch = (ev) => {
-    ev.preventDefault();
-    console.log('funciona');
+  ev.preventDefault();
+  console.log("funciona");
+  const valueDesc = descCocktail.value;
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${valueDesc}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      listCocktailData = data.drinks;
+      console.log(listCocktailData);
 
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-    .then(response => response.json())
-    .then(data=>{
-        console.log(data);
-        listCocktailData=data.drinks
-        console.log(listCocktailData)
-        
-    })
-}
+      renderListCocktail(listCocktailData);
+    });
+};
 
-
-btnSearch.addEventListener('click',handleClickSearch); //evento click en botón buscar
+//2- Evento click en botón buscar
+btnSearch.addEventListener("click", handleClickSearch);
