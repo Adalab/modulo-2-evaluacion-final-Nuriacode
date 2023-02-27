4d34e2226ca6f942a6cd8d31090807c7bb6e0c6e
Nuria Castaño <codesalnuria@gmail.com>
2023-02-27 12:43:12 +0100

    add readme, and style changes in each list and article

--- README.md
+++ README.md
@@ -1,124 +1,49 @@
-![Adalab](https://beta.adalab.es/resources/images/adalab-logo-155x61-bg-white.png)
+# Buscador de Cocktails
 
-# Adalab web starter kit
+¡Hola! :blush:
 
-Ahoy! Este es nuestro Starter Kit creado en **node y gulp**. ¿Y qué es un Starter kit? Pues es una **plantilla de proyecto con funcionalidades preinstaladas y preconfiguradas**.
+Este es mi ejercicio de evaluación del módulo 2 de Adalab, donde tiene preferencia de evaluación el código escrito en JS. Consiste en crear una página web donde se puedan buscar tus cócteles favoritos y poder agregarlos y quitarlos de una lista de favoritos.
 
-Este Kit incluye un motor de plantillas HTML, el preprocesador SASS y un servidor local y muchas cosas más. El Kit nos ayuda a trabajar más cómodamente, nos automatiza tareas.
+## Funcionamiento
 
-En el Kit hay 3 tipos de ficheros y carpetas:
+### Setup de la página
 
-- Los ficheros que están sueltos en la raíz del repositorio, como gulpfile.js, package.json... Son la configuración del proyecto y no necesitamos modificarlos.
-- La carpeta `src/`: son los ficheros de nuestra página web, como HTML, CSS, JS...
-- Las carpetas `public/` y `docs/`, que son generadas automáticamente cuando arrancamos el proyecto. El Kit lee los ficheros que hay dentro de `src/`, los procesa y los genera dentro de `public/` y `docs/`.
+Al abrir la página, en la columna de "Lista de Cócteles", aparecen por defecto un listado de cócteles de margarita, ya que así lo indicaba el ejercicio. Para que aparezcan los cócteles, la aplicación se conecta a la API abierta de TheCocktailDB y se muestra la imagen y el nombre de cada cocktail. En el caso de que no hubiese imagen, se muestra otra por defecto.
 
-## Guía de inicio rápido
+### Búsqueda
 
-> **NOTA:** Necesitas tener instalado [Node JS](https://nodejs.org/) para trabajar con este Starter Kit:
+En la parte del formulario, se debe escribir el nombre de tu cocktail favorito, y después de hacer click en el botón de buscar, mostrará un listado de cocteles que contengan esa misma palabra.
 
-### Pasos a seguir cada vez que queremos arrancar un proyecto desde cero:
+### Añadir a lista de favoritos
 
-1. **Crea tu propio repositorio.**
-1. Descarga este **Starter kit desde GitHub**.
-   - No recomendamos que clones este repo ya que no podrás añadir commits.
-1. **Copia todos los ficheros** de este Starter kit en la carpeta raíz de tu repositorio.
-   - Recuerda que debes copiar **también los ficheros ocultos**.
-   - Si has decidido clonar este repo, no debes copiar la carpeta `.git`. Si lo haces estarás machacando tu propio repositorio.
-1. **Abre una terminal** en la carpeta raíz de tu repositorio.
-1. **Instala las dependencias** locales ejecutando en la terminal el comando:
+Para añadir un coctel al listado de favoritos, se debe hacer click en uno de los cócteles y este cambiará el color del fondo y de letra, además de añadirlo en la lista de favoritos.
 
-```bash
-npm install
-```
+### Almacenamiento local
 
-### Pasos para arrancar el proyecto:
+Esta parte ayuda a que si en algún momento actualizamos la página, la lista de favoritos seguirá llena con los cócteles que has añadido anteriormente.
 
-Una vez hemos instalado las dependencias, vamos a arrancar el proyecto. **El proyecto hay que arrancarlo cada vez que te pongas a programar.** Para ello ejecuta el comando:
+### Botón Reset
 
-```bash
-npm start
-```
+Este botón, limpia el input sin ninguna palabra, borra los cocteles añadiso en la lista de favoritos y vuelve a mostrar la lista de cocteles de margaritas, que es como se inicia desde el principio.
 
-Este comando:
+### Bonús de la página
 
-- **Abre una ventana de Chrome y muestra tu página web**, al igual que hace el plugin de VS Code Live Server (Go live).
-- También **observa** todos los ficheros que hay dentro de la carpeta `src/`, para que cada vez que modifiques un fichero **refresca tu página en Chrome**.
-- También **procesa los ficheros** HTML, SASS / CSS y JS y los **genera y guarda en la carpeta `public/`**. Por ejemplo:
-   - Convierte los ficheros SASS en CSS.
-   - Combina los diferentes ficheros de HTML y los agrupa en uno o varios ficheros HTML.
+Al añadir a la lista de favoritos un cocktail se le añade una X para poder eliminarlo de la lista haciendo click en ella.
 
-Después de ejecutar `npm start` ya puedes empezar a editar todos los ficheros que están dentro de la carpeta `src/` y programar cómodamente.
+## Herramientas utlilizadas
 
-### Pasos para publicar el proyecto en GitHub Pages:
+En este proyecto se ha utilizado:
 
-Para generar tu página para producción ejecuta el comando:
+- Adalab web starter kit
+- HTML
+- SCSS
+- JavaScript
+- Estructura BEM
+- Vscode
+- GIT
+- GitHub
+- Gulp
 
-```bash
-npm run docs
-```
+## Autora
 
-Y a continuación:
-
-1. Sube a tu repo la carpeta `docs/` que se te acaba de generar.
-1. Entra en la pestaña `settings` de tu repo.
-1. Y en el apartado de GitHub Pages activa la opción **master branch /docs folder**.
-1. Y ya estaría!!!
-
-Además, los comandos:
-
-```bash
-npm run push-docs
-```
-o
-
-```bash
-npm run deploy
-```
-
-son un atajo que nos genera la versión de producción y hace push de la carpeta `docs/` del tirón. Te recomendamos ver el fichero `package.json` para aprender cómo funciona.
-
-## Flujo de archivos con Gulp
-
-Estas tareas de Gulp producen el siguiente flujo de archivos:
-
-![Gulp flow](./gulp-flow.png)
-
-## `gulpfile.js` y `config.json`
-
-Nuestro **gulpfile.js** usa el fichero `config.json` de configuración con las rutas de los archivos a generar / observar.
-
-De esta manera separarmos las acciones que están en `gulpfile.js` de la configuración de las acciones que están en `config.json`.
-
-## Estructura de carpetas
-
-La estructura de carpetas tiene esta pinta:
-
-```
-src
- ├─ api // los ficheros de esta carpeta se copian en public/api/
- |  └─ data.json
- ├─ images
- |  └─ logo.jpg
- ├─ js // los ficheros de esta carpeta se concatenan en el fichero main.js y este se guarda en public/main.js
- |  ├─ main.js
- |  └─ events.js
- ├─ scss
- |  ├─ components
- |  ├─ core
- |  ├─ layout
- |  └─ pages
- └─ html
-    └─ partials
-```
-
-> **NOTA:** Los partials de HTML y SASS del proyecto son orientativos. Te recomendamos usar los que quieras, y borrar los que no uses.
-
-## Vídeotutoriales del Starter kit
-
-- [Qué es, trabajar con la versión de desarrollo y rutas relativas](https://www.youtube.com/watch?v=XwvhXvBijos)
-- [Migración de un proyecto, trabajar con la versión de producción y GitHub Pages](https://www.youtube.com/watch?v=qqGClcgt9Uc)
-- [Motor de plantillas](https://www.youtube.com/watch?v=4GwXOJ045Zg)
-
-## Falta algo?
-
-Echas de menos que el kit haga algo en concreto? Pidelo sin problema a través de las issues o si te animas a mejorarlo mándanos un PR :)
+Nuria Castaño
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 //Global variables: getting from HTML
 const input = document.querySelector(".js_descSearch"); //what of someone type insside input
 const placeholder =
@@ -8,13 +7,14 @@ const placeholder =
 const urlCocktails = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`;
 const btnSearch = document.querySelector(".js_btnSearch"); //search button
 const btnReset = document.querySelector(".js_btnReset"); //reset button
+const textMsg = document.querySelector(".js_msg");
 const cocktails = document.querySelector(".js_listCocktail"); //cocktails list
 const favCocktail = document.querySelector(".js_listFavorites"); //Favorites cocktail list
 
 let listCocktailData = []; //cocktail array
 let listFavCocktailData = []; //favorites cocktail array
 
-//localstorage
+//Getting localstorage
 const cocktailStoraged = JSON.parse(localStorage.getItem("Cocktail"));
 console.log(listFavCocktailData);
 
@@ -43,14 +43,14 @@ function initialFetch() {
     });
 }
 
-//Function that render the list of the array of objects 
+//Function that render the list of the array of objects
 function renderListCocktail(listCocktailData) {
   cocktails.innerHTML = "";
   for (const cocktail of listCocktailData) {
     cocktails.innerHTML += renderCocktail(cocktail);
   }
   eventToCocktail();
-};
+}
 
 //Function that render each object (cocktail) from the server.
 function renderCocktail(cocktail) {
@@ -59,56 +59,64 @@ function renderCocktail(cocktail) {
   }
   let html = "";
   html += `<li> 
-  <article class="js_liElement" id="${cocktail.idDrink}">
+  <article class="article js_liElement" id="${cocktail.idDrink}">
     <img class="img" src="${cocktail.strDrinkThumb}" alt="Foto Cocktail ${cocktail.strDrink}"> 
     <h3>${cocktail.strDrink}</h3>
   </article>
   </li>`;
   return html;
-};
+}
 
-//función pinta la lista de favoritos
+//function of the favourites list
 function renderFavListCocktail(listFavCocktail) {
   favCocktail.innerHTML = "";
   for (const item of listFavCocktail) {
     favCocktail.innerHTML += renderFavCocktail(item);
   }
-  eventoToFavCocktail ();
+  eventoToFavCocktail();
 }
 
-//función manejadora de click buscar
+//click search handler function
 const handleClickSearch = (ev) => {
-  ev.preventDefault();
   //console.log("funciona");
   const valueDesc = input.value;
-  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${valueDesc}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      listCocktailData = data.drinks;
-      console.log(listCocktailData);
+  if (valueDesc === "") {
+    textMsg.innerHTML = "";
+  } else {
+    ev.preventDefault();
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${valueDesc}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        listCocktailData = data.drinks;
+        console.log(listCocktailData);
 
-      renderListCocktail(listCocktailData);
-    });
+        renderListCocktail(listCocktailData);
+      });
+  }
 };
 
 //2- Event click on the search button.
 btnSearch.addEventListener("click", handleClickSearch);
 
-//3- Favoritos: 3.1-seleccionar todos los elementos de la lista: añadir una clase en todos los li. 3.2-
 
-//función manejadora de eventToCocktail
+
+//eventToCocktail handler function
 const handleClickCocktail = (ev) => {
   console.log("Me has seleccionado");
   console.log(ev.currentTarget.id);
   ev.currentTarget.classList.toggle("selected");
   const idSelected = ev.currentTarget.id;
-  //buscar el objeto del coctel seleccionado en toda la lista de cocteles
-  const cocktailSelected = listCocktailData.find((cocktail) => cocktail.idDrink === idSelected);
+  //search for the object of the selected cocktail in the entire cocktail list
+  const cocktailSelected = listCocktailData.find(
+    (cocktail) => cocktail.idDrink === idSelected
+  );
   console.log(cocktailSelected);
-  //buscar la posición del coctel en mi lista de favoritos. si es -1 no está en esa lista y hay que ponerlo
-  const indexCocktail = listFavCocktailData.findIndex((cocktail) => cocktail.idDrink === idSelected);
+  //look for the position of the cocktail in my list of favourites. if it is -1 it is not in that list and you have to put it in
+  const indexCocktail = listFavCocktailData.findIndex(
+    (cocktail) => cocktail.idDrink === idSelected
+  );
   console.log(indexCocktail);
   if (indexCocktail === -1) {
     listFavCocktailData.push(cocktailSelected);
@@ -116,16 +124,16 @@ const handleClickCocktail = (ev) => {
   } else {
     listFavCocktailData.splice(indexCocktail, 1);
   }
-  //pintar la lista de favoritos con función renderFavListCocktail
+  //paint the favourites list with function renderFavListCocktail
   renderFavListCocktail(listFavCocktailData);
-  //4Localstorage: qué voy a guardar y en qué momento. Guardar la lista de favoritos cuando se haga click en uno de los elementos.
+  //4Localstorage:  what to save and when to save it. Save the list of favourites when one of the items is clicked.
   localStorage.setItem("Cocktail", JSON.stringify(listFavCocktailData));
 };
 
-//función que se ejecuta una vez cogidos los datos del servidor e incluye un evento click en cada coctel
+//function that is executed once the data is taken from the server and includes a click event in each cocktail.
 const eventToCocktail = () => {
   const liElementCocktail = document.querySelectorAll(".js_liElement"); //elementos de li
-  //hacer bucle para que me seleccione todos los elementos y añadirle un evento click a cada uno de ellos
+  //loop to select all elements and add a click event to each of them
   for (const item of liElementCocktail) {
     item.addEventListener("click", handleClickCocktail);
   }
@@ -146,8 +154,7 @@ function handleClickReset(ev) {
 
 btnReset.addEventListener("click", handleClickReset);
 
-
-//1-pintar la X (dentro del article) cuando entra en el array de favoritos. función pintar 
+//1-paint the X (inside the article) when it enters the favourites array. function paint
 //
 
 //Function that render each object (cocktail) from the server.
@@ -157,35 +164,33 @@ function renderFavCocktail(cocktail) {
   }
   let html = "";
   html += `<li> 
-  <article class="js_liElement" id="${cocktail.idDrink}">
+  <article class="article js_liElement" id="${cocktail.idDrink}">
     <p class="remove js_removeFav" id="${cocktail.idDrink}">x</p>
     <img class="img" src="${cocktail.strDrinkThumb}" alt="Foto Cocktail ${cocktail.strDrink}"> 
     <h3>${cocktail.strDrink}</h3>
   </article>
   </li>`;
   return html;
-};
-
-
-function eventoToFavCocktail (){
-  const removeFav = document.querySelectorAll('.js_removeFav');
-    for (const item of removeFav){
-      item.addEventListener('click', handeleClickRemoveFav);}
 }
 
+function eventoToFavCocktail() {
+  const removeFav = document.querySelectorAll(".js_removeFav");
+  for (const item of removeFav) {
+    item.addEventListener("click", handeleClickRemoveFav);
+  }
+}
 
-function handeleClickRemoveFav(ev){
-  console.log(ev.target.id);
-  const idFavSelected = ev.target.id;
-  const indexFavCocktail = listFavCocktailData.findIndex((favCocktail) => favCocktail.idDrink === idFavSelected);
+function handeleClickRemoveFav(ev) {
+  console.log(ev.currentTarget.id);
+  const idFavSelected = ev.currentTarget.id;
+  const indexFavCocktail = listFavCocktailData.findIndex(
+    (favCocktail) => favCocktail.idDrink === idFavSelected
+  );
   console.log(indexFavCocktail);
-  if (!indexFavCocktail){
+  if (!indexFavCocktail) {
     listFavCocktailData.splice(indexFavCocktail, 1);
   }
   localStorage.removeItem("Cocktail");
   renderFavListCocktail(listFavCocktailData);
   renderListCocktail(listCocktailData);
 }
-
-
-
